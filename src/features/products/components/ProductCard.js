5c78f2fad84b4d6075cfccc12be3ff273503637b
miguel <miguelvaladezr29@gmail.com>
2026-04-21 20:29:36 -0600

    Correcting catalog using Dimension module

--- src/features/products/components/ProductCard.js
+++ src/features/products/components/ProductCard.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 
 export const ProductCard = ({ product }) => {
 
@@ -10,40 +10,46 @@ export const ProductCard = ({ product }) => {
         ? { uri: product.image }
         : product.image;
 
+
+
     return (
-        <TouchableOpacity onPress = {() => navigation.navigate ('ProductDetail', {product})}>
-        <View style={styles.card}>
-            <Image
-                source={imageSource}
-                style={styles.image}
-                resizeMode="contain"
-            />
-            <View style={styles.info}>
-                <Text style={styles.title} numberOfLines={2}>{product.title}</Text>
-                <Text style={styles.seller}>Seller: {product.seller}</Text>
-                <View style={styles.ratingContainer}>
-                    <Text style={styles.ratingText}>⭐ {product.rating.rate}</Text>
-                    <Text style={styles.countText}>({product.rating.count})</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('ProductDetail', { product })}>
+            <View style={styles.card}>
+                <Image
+                    source={imageSource}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+                <View style={styles.info}>
+                    <Text style={styles.title} numberOfLines={2}>{product.title}</Text>
+                    <Text style={styles.seller}>Seller: {product.seller}</Text>
+                    <View style={styles.ratingContainer}>
+                        <Text style={styles.ratingText}>⭐ {product.rating.rate}</Text>
+                        <Text style={styles.countText}>({product.rating.count})</Text>
+                    </View>
+                    <Text style={styles.price}>${product.price.toFixed(2)}</Text>
                 </View>
-                <Text style={styles.price}>${product.price.toFixed(2)}</Text>
             </View>
-        </View>
         </TouchableOpacity>
     );
 };
+
+const { width } = Dimensions.get('window');
+const cardWidth = (width / 2) - 15;
 
 const styles = StyleSheet.create({
     card: {
         backgroundColor: '#fff',
         borderRadius: 12,
-        margin: 8,
+        margin: 7,
         flex: 1,
         elevation: 4,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 4,
-        overflow: 'hidden'
+        overflow: 'hidden',
+        width: cardWidth
     },
     image: { width: '100%', height: 140, backgroundColor: '#fff', marginTop: 10 },
     info: { padding: 12 },
